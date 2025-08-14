b28d32466e6c3b6a3546ba08c4241e5d189bd4ba
Misbahudin <mizzc0d3@gmail.com>
2025-08-14 20:16:38 +0700

    add setting for docker build when using proxy

--- seting-proxy/set_proxy.sh
+++ seting-proxy/set_proxy.sh
@@ -1,26 +1,22 @@
-# Bersihkan baris proxy lama
+#!/bin/zsh
+
 sed -i '/export no_proxy=/d' ~/.zshrc
 sed -i '/export https_proxy=/d' ~/.zshrc
 sed -i '/export http_proxy=/d' ~/.zshrc
 sed -i '/export all_proxy=/d' ~/.zshrc
 
-# Tambahkan baris proxy baru
 echo 'export no_proxy="localhost,127.0.0.1,::1"' >> ~/.zshrc
 echo 'export https_proxy="http://127.0.0.1:10809"' >> ~/.zshrc
 echo 'export http_proxy="http://127.0.0.1:10809"' >> ~/.zshrc
 echo 'export all_proxy="socks5://127.0.0.1:10808"' >> ~/.zshrc
-source ~/.zshrc
 
-# Snap
+echo "Memerlukan password untuk konfigurasi sistem (APT, Snap)..."
 sudo snap set system proxy.http="http://127.0.0.1:10809"
 sudo snap set system proxy.https="http://127.0.0.1:10809"
-
-# APT
 echo 'Acquire::http::Proxy "http://127.0.0.1:10809/";' | sudo tee /etc/apt/apt.conf.d/01proxy > /dev/null
 echo 'Acquire::https::Proxy "http://127.0.0.1:10809/";' | sudo tee -a /etc/apt/apt.conf.d/01proxy > /dev/null
 
-# Git
 git config --global http.proxy http://127.0.0.1:10809
 git config --global https.proxy http://127.0.0.1:10809
 
-echo "Proxy settings applied successfully!"
+echo "Proxy settings applied! Jalankan 'source ~/.zshrc' atau buka terminal baru."
--- seting-proxy/unset_proxy.sh
+++ seting-proxy/unset_proxy.sh
@@ -1,25 +1,20 @@
-# Hapus baris proxy dari ~/.zshrc
+#!/bin/zsh
+
 sed -i '/export no_proxy=/d' ~/.zshrc
 sed -i '/export https_proxy=/d' ~/.zshrc
 sed -i '/export http_proxy=/d' ~/.zshrc
 sed -i '/export all_proxy=/d' ~/.zshrc
-source ~/.zshrc
 
-# Unset environment variable di shell aktif
 unset no_proxy
 unset https_proxy
 unset http_proxy
 unset all_proxy
 
-# APT
+echo "Memerlukan password untuk konfigurasi sistem (APT, Snap)..."
 sudo rm -f /etc/apt/apt.conf.d/01proxy
-
-# Snap
 sudo snap unset system proxy.http
 sudo snap unset system proxy.https
-sudo systemctl restart snapd
 
-# Git
 git config --global --unset http.proxy
 git config --global --unset https.proxy
 
